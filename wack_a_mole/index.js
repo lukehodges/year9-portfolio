00cfd37d4296ed6aeec201121edeb57d9d88bd2f
luke hodges <58270858+lukehodges@users.noreply.github.com>
2021-03-02 15:24:12 +0000

    added mole hits counter

--- wack_a_mole/index.js
+++ wack_a_mole/index.js
@@ -1,6 +1,10 @@
+var count=0
+var timer=0
 function move(obj){
-    var x = Math.trunc(Math.random()*1000)
-    var y = Math.trunc(Math.random()*1000)
+    count += 1
+    var x = Math.trunc(Math.random()*600)
+    var y = Math.trunc(Math.random()*600)
     console.log(x,y)
     document.getElementById("mole").style.margin=x,y
+    document.getElementById("count").innerHTML='moles hits: '+count;
 }
